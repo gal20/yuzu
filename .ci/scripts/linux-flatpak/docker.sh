5f9c9a558571ab756decdf00079e9efd1bbabfce
gal20 <71563441+gal20@users.noreply.github.com>
2020-10-08 18:21:47 +0300

    Update docker.sh

--- .ci/scripts/linux-flatpak/docker.sh
+++ .ci/scripts/linux-flatpak/docker.sh
@@ -31,8 +31,8 @@ GPG_KEY="/tmp/gpg.key"
 #gpg2 --import "$GPG_KEY"
 
 # Set permissions
-#chown -R yuzu "$YUZU_SRC_DIR"
-#chmod -R 700 "$YUZU_SRC_DIR"
+chown -R yuzu "$YUZU_SRC_DIR"
+chmod -R 700 "$YUZU_SRC_DIR"
 
 # Mount our flatpak repository
 # -o reconnect and -o ServerAliveInterval ensure that 
@@ -41,15 +41,15 @@ mkdir -p "$REPO_DIR"
 #sshfs "$FLATPAK_SSH_USER@$FLATPAK_SSH_HOSTNAME:$SSH_DIR" "$REPO_DIR" -C -p "$FLATPAK_SSH_PORT" -o IdentityFile="$SSH_KEY" -o ServerAliveInterval=60 -o "reconnect" -o auto_cache -o no_readahead
 
 # setup ccache location
-#chown -R yuzu "$HOME/.ccache"
-#chmod -R 700 "$HOME/.ccache"
+chown -R yuzu "$HOME/.ccache"
+chmod -R 700 "$HOME/.ccache"
 mkdir -p "$STATE_DIR"
-#chown -R yuzu "$STATE_DIR"
-#chmod -R 700 "$STATE_DIR"
+chown -R yuzu "$STATE_DIR"
+chmod -R 700 "$STATE_DIR"
 ln -sv --force $HOME/.ccache "$STATE_DIR/ccache"
 
 # Set ccache permissions
-#chmod -R 700 "$STATE_DIR/ccache"
+chmod -R 700 "$STATE_DIR/ccache"
 
 # Build the yuzu flatpak
 #flatpak-builder -v --jobs=4 --ccache --force-clean --state-dir="$STATE_DIR" --gpg-sign="$FLATPAK_GPG_PUBLIC_KEY" --repo="$REPO_DIR" "$BUILD_DIR" "/tmp/org.yuzu.$REPO_NAME.json"
