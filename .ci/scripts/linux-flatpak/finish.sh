871fbf666c4d86aa1b3942afbc363c5dce052051
gal20 <71563441+gal20@users.noreply.github.com>
2020-10-08 16:35:00 +0300

    Update finish.sh

--- .ci/scripts/linux-flatpak/finish.sh
+++ .ci/scripts/linux-flatpak/finish.sh
@@ -5,5 +5,5 @@ REPO_DIR="$YUZU_SRC_DIR/repo"
 
 # When the script finishes, unmount the repository and delete sensitive files,
 # regardless of whether the build passes or fails
-umount "$REPO_DIR" || true
+#umount "$REPO_DIR" || true
 rm -rf "$REPO_DIR" "/tmp/*" || true
