1e6aa92638ab5dd3e84e4745072b627b3d5291df
gal20 <71563441+gal20@users.noreply.github.com>
2020-10-08 19:30:52 +0300

    Update exec.sh

--- .ci/scripts/linux-flatpak/exec.sh
+++ .ci/scripts/linux-flatpak/exec.sh
@@ -10,5 +10,5 @@ sudo chown -R 1027 "$HOME/.ccache"
 sudo chown -R 1027 $(pwd)
 docker run --env-file .ci/scripts/linux-flatpak/azure-ci.env --env-file .ci/scripts/linux-flatpak/azure-ci-flatpak.env -v $(pwd):/yuzu -v "$HOME/.ccache":/home/yuzu/.ccache -v "$HOME/.ssh":/home/yuzu/.ssh --privileged meirod/build-environments:linux-flatpak /bin/bash -ex /yuzu/.ci/scripts/linux-flatpak/docker.sh
 #sudo chown -R $UID "$HOME/.ssh"
-sudo chown -R $UID" $HOME/.ccache"
+sudo chown -R $UID "$HOME/.ccache"
 sudo chown -R $UID $(pwd)
